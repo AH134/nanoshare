87e1a6a3ed8040e2dd12e704dc546b132c12af22
AH134 <73412379+AH134@users.noreply.github.com>
2026-07-16 18:45:14 -0700

    refactor createadmin to use UserRepository

---
+++ cmd/createadmin/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+
+	"github.com/AH134/nanoshare/internal/database"
+)
+
+func main() {
+	username := flag.String("username", "admin", "admin username")
+	password := flag.String("password", "admin", "admin password")
+	flag.Parse()
+
+	db, err := database.New("./data/nanoshare.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	userRepo := database.NewUserRepository(db)
+
+	if err = userRepo.CreateAdmin(*username, *password); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("admin user created")
+}
