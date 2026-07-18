3d938d3dfe90b92f6d23102ee9a6357b83ed62bb
AH134 <73412379+AH134@users.noreply.github.com>
2026-07-18 12:16:23 -0700

    update links table and create links repository

--- internal/database/migrations/20260718082917_links.sql
+++ internal/database/migrations/20260718082917_links.sql
@@ -1,7 +1,13 @@
 -- +goose Up
 CREATE TABLE links (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    file_id INTEGER NOT NULL REFERENCES files(id) ON DELETE CASCADE
+    file_id INTEGER NOT NULL REFERENCES files(id) ON DELETE CASCADE,
+    token TEXT NOT NULL UNIQUE,
+    max_downloads INTEGER,
+    download_count INTEGER NOT NULL DEFAULT 0,
+    created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    expires_at DATETIME NOT NULL,
+    revoked_at DATETIME
 );
 
 -- +goose Down
