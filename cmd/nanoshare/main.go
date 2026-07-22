952ff363ccf15e01c2b8c73968f9b91ba87d5c0c
AH134 <73412379+AH134@users.noreply.github.com>
2026-07-21 23:33:02 -0700

    remove unused function

--- cmd/nanoshare/main.go
+++ cmd/nanoshare/main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
 	"log"
 	"time"
 
@@ -13,31 +11,6 @@ import (
 	"github.com/AH134/nanoshare/internal/storage"
 	"github.com/alexedwards/scs/sqlite3store"
 )
-
-func temp(db *database.DB) {
-	linkRepo := database.NewLinkRepository(db)
-	// link := &database.Link{
-	// 	FileID:    1,
-	// 	Token:     "test-token-123",
-	// 	ExpiresAt: time.Now().Add(24 * time.Hour),
-	// }
-
-	// if err := linkRepo.Create(link); err != nil {
-	// 	log.Fatal(err)
-	// }
-
-	l, err := linkRepo.GetByToken("test-token-123")
-	if err != nil {
-		log.Fatal(err)
-	}
-	j, err := json.MarshalIndent(l, "", "  ")
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Printf("%+v\n", l)
-	fmt.Printf("%s\n", j)
-
-}
 
 func main() {
 	cfg, err := config.Load()
@@ -54,8 +27,6 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	// temp(db)
 
 	if err := database.RunMigrations(db.Conn()); err != nil {
 		log.Fatal(err)
